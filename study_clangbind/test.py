66cca17b2e05497b7aa4fcb24e391175792e24fa
purasutiku1123@yahoo.co.jp <purasutiku1123@yahoo.co.jp>
2020-05-09 05:31:25 +0900

    clang勉強ソース追加

---
+++ study_clangbind/test.py
@@ -0,0 +1,19 @@
+import clang.cindex
+from clang.cindex import Index
+from clang.cindex import Config
+
+Config.set_compatibility_check(False)
+Config.set_library_file("/usr/lib/x86_64-linux-gnu/libclang-9.so")
+
+def print_method_area(node):
+    if (node.kind.name == 'FUNCTION_DECL'):
+        print("file : {0}".format(node.extent.start.file))
+        print("function : {0}".format(node.displayname))
+        print(" from line:{0} column:{1}".format(node.extent.start.line, node.extent.start.column))
+        print(" to   line:{0} column:{1}".format(node.extent.end.line, node.extent.end.column))
+    for child in node.get_children():
+        print_method_area(child)
+
+index = Index.create()
+tu = index.parse("test.cpp")
+print_method_area(tu.cursor)
